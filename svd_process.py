aef63f96a2fbb33a76e79ed1569efb221aea207f
LordGedelicious <gede.coolguy@gmail.com>
2021-11-09 23:21:32 +0700

    Update svd_process.py

--- svd_process.py
+++ svd_process.py
@@ -1,3 +1,4 @@
+from itertools import count
 import re
 from mpmath.matrices.eigen import eig
 import numpy as np
@@ -15,34 +16,36 @@ def left_singular(matrix):
     length, width = matrix.shape
     identity = Matrix.eye(length)
     x = symbols('x')
-    print(identity)
     for i in range(0,length):
         for j in range(0,width):
             if (identity[i,j] == 1):
                 identity[i,j] = x
-    print(identity)
     result = identity - matrix
-    print(matrix)
-    print(result)
-    print(result.det())
     eigenvalues = sympy.solve(result.det(),x)
-    print(eigenvalues)
     eigenvalues_np = np.array(eigenvalues).astype(np.int64)
     eigenvalues_np[::-1].sort()
-    print(eigenvalues_np)
     eigenvalues = sympy.Matrix(eigenvalues_np)
-    print(eigenvalues)
-    print(sympy.shape(eigenvalues))
     zeroMatrix = zeros(length,1)
-    print(zeroMatrix)
+    final_eigenvector = sympy.Matrix([[]])
     for i in range(0,sympy.shape(eigenvalues)[0]): #ini baru bisa manggil eigen value pertama
         for j in range(0,length):
             for k in range(0,width):
-                if (identity[j,k] == x):
+                if (identity[j,k] == x or identity[j,k] == eigenvalues[i-1,0]):
                     identity[j,k] = eigenvalues[i,0]
         subtractToEigen = identity - matrix
-        print(subtractToEigen)
-        newsubtractToEigen = subtractToEigen.col_insert(length,zeroMatrix)
-        print(newsubtractToEigen)
+        solutions, params = subtractToEigen.gauss_jordan_solve(zeroMatrix)
+        for i in params:
+            sum = 0
+            partial_solution_temp = solutions.xreplace({i:1})
+            partial_solution = partial_solution_temp.xreplace({i:0 for i in params})
+            print(partial_solution)
+            for i in partial_solution:
+                sum += i ** 2
+            sum = (sum ** 0.5) ** (-1)
+            partial_solution = sum * partial_solution
+            final_eigenvector = (final_eigenvector.col_insert(length,partial_solution))
+            print(final_eigenvector)
+
+
 
 left_singular(sample)
