f9f973f9f60282f90e0f6cbcc3dd2fd0c1c029b3
Muhammad Helmi Hibatullah <helmihibatullah52@gmail.com>
2021-11-10 07:03:18 +0700

    refactor and add new function

--- svd_process.py
+++ svd_process.py
@@ -1,51 +1,109 @@
-from itertools import count
-import re
-from mpmath.matrices.eigen import eig
 import numpy as np
-import cv2
-from numpy.core.fromnumeric import shape
-from sympy import symbols, Matrix
-from sympy.core.symbol import Symbol
+from numpy.core.fromnumeric import transpose
 import sympy
-from sympy.matrices.dense import zeros
-from sympy.solvers import solve
+from sympy import symbols, Matrix, solve, zeros
 
-sample = np.array([[3,-2,0],[-2,3,0],[0,0,5]])
 
-def left_singular(matrix):
+def singular_vectors(matrix, left=True):
+    """ Menghitung matriks singular kiri atau kanan.\n
+        Untuk menghitung matriks singular kiri, 
+            Argumen matrix berupa matriks AAT dan left = True.\n
+        Untuk menghitung matriks singular kanan, 
+            Argumen matrix berupa matriks ATA dan left = False.\n
+        
+        Prekondisi: matrix berupa matriks persegi."""
+        
+    length, width = matrix.shape
+    
+    x = symbols('x')
+    identity = create_identity(matrix)
+    eigen_val = eigen_values(matrix)
+    zero_matrix = zeros(length,1)
+    final_eigen_vector = Matrix([[]])
+    
+    # menghitung vektor eigen
+    for i in range(sympy.shape(eigen_val)[0]):
+        # subtitusi x pada identity menjadi nilai eigennya
+        for j in range(length):
+            for k in range(width):
+                if (identity[j,k] == x or identity[j,k] == eigen_val[i-1,0]):
+                    identity[j,k] = eigen_val[i,0]
+        
+        identity_subtract_matrix = identity - matrix
+        solutions, params = identity_subtract_matrix.gauss_jordan_solve(zero_matrix)
+        for p in params:
+            # subtitusi 1 ke parameter pada solutions
+            partial_solution_temp = solutions.xreplace({p:1})
+            partial_solution = partial_solution_temp.xreplace({p:0 for _ in params})
+            
+            # normalisasi vektor
+            vector_len = 0
+            for q in partial_solution:
+                vector_len += q ** 2
+            vector_len = (vector_len ** 0.5) ** (-1)
+            partial_solution *= vector_len
+            
+            final_eigen_vector = final_eigen_vector.col_insert(length,partial_solution)
+        print(final_eigen_vector)
+    if left:
+        return final_eigen_vector
+    else:
+        return transpose(final_eigen_vector)
+
+
+def eigen_values(matrix):
+    """ Fungsi mengembalikan matriks berisi
+        nilai eigen.
+        
+        Prekondisi: matrix berupa matriks persegi."""
+        
+    x = symbols('x')
+    identity = create_identity(matrix)
+    
+    identity_subtract_matrix = identity - matrix
+    eigen_val = solve(identity_subtract_matrix.det(),x)
+    eigen_val_np = np.array(eigen_val).astype(np.int64)
+    eigen_val_np[::-1].sort()
+    eigen_val = Matrix(eigen_val_np)
+
+    return eigen_val
+
+
+def create_identity(matrix):
+    """ Fungsi mengembalikan matriks identitas
+        yang tiap angka 1 diganti dengan simbol x.
+        
+        Prekondisi: matrix berupa matriks persegi."""
+        
     length, width = matrix.shape
     identity = Matrix.eye(length)
     x = symbols('x')
-    for i in range(0,length):
-        for j in range(0,width):
+    for i in range(length):
+        for j in range(width):
             if (identity[i,j] == 1):
                 identity[i,j] = x
-    result = identity - matrix
-    eigenvalues = sympy.solve(result.det(),x)
-    eigenvalues_np = np.array(eigenvalues).astype(np.int64)
-    eigenvalues_np[::-1].sort()
-    eigenvalues = sympy.Matrix(eigenvalues_np)
-    zeroMatrix = zeros(length,1)
-    final_eigenvector = sympy.Matrix([[]])
-    for i in range(0,sympy.shape(eigenvalues)[0]): #ini baru bisa manggil eigen value pertama
-        for j in range(0,length):
-            for k in range(0,width):
-                if (identity[j,k] == x or identity[j,k] == eigenvalues[i-1,0]):
-                    identity[j,k] = eigenvalues[i,0]
-        subtractToEigen = identity - matrix
-        solutions, params = subtractToEigen.gauss_jordan_solve(zeroMatrix)
-        for i in params:
-            sum = 0
-            partial_solution_temp = solutions.xreplace({i:1})
-            partial_solution = partial_solution_temp.xreplace({i:0 for i in params})
-            print(partial_solution)
-            for i in partial_solution:
-                sum += i ** 2
-            sum = (sum ** 0.5) ** (-1)
-            partial_solution = sum * partial_solution
-            final_eigenvector = (final_eigenvector.col_insert(length,partial_solution))
-            print(final_eigenvector)
+    
+    return identity
 
 
-
-left_singular(sample)
+def singular_values(matrix):
+    """ Fungsi mengembalikan matriks berisi
+        nilai-nilai singular.
+        
+        Prekondisi: matrix berupa matriks persegi."""
+        
+    length, width = matrix.shape
+    final_singular_matrix = [[0 for _ in range(width)] for _ in range(length)]
+    eigen_val = eigen_values(matrix)
+    
+    for i in range(min(length, width)):
+        if eigen_val[i] != 0:
+            final_singular_matrix[i][i] = eigen_val[i] ** 0.5
+            
+    final_singular_matrix = Matrix(final_singular_matrix)
+    
+    return final_singular_matrix
+    
+    
+sample = np.array([[10, 0, 2], [0, 10, 4], [2, 4, 2]])
+print(singular_values(sample))
