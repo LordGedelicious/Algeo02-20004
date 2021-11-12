617b769cf09bfe50d166cbab524157287199871e
blueguy42 <ahmadalfanihandoyo@hotmail.com>
2021-11-12 13:38:28 +0700

    Update compressor.py
    confused as to why png's compress to the same size, turns out to just be PIL things

--- src/compressor.py
+++ src/compressor.py
@@ -1,9 +1,11 @@
 from PIL import Image
 from os.path import join
 
+# PIL Save Documentation: https://pillow.readthedocs.io/en/stable/handbook/image-file-formats.html
+
 
 def compressor(original_realpath, compressed_realpath, file_name, cprate, prefix):
     picture = Image.open(join(original_realpath, file_name))
 
-    picture.save(join(compressed_realpath, prefix+file_name),optimize=True,quality=cprate) 
+    picture.save(join(compressed_realpath, prefix+file_name),quality=cprate) 
     picture.close()
